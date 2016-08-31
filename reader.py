f21de7cf0668c0c9e25fc33a00e65de0ca4cebf7
bFraley <bFraley@users.noreply.github.com>
2016-08-31 15:02:31 -0400

    initial success on challenge

---
+++ reader.py
@@ -0,0 +1,97 @@
+# reader.py for store_data.json file
+
+import json
+
+# Reading data back
+with open('store_data.json', 'r') as f:
+     data_file = json.load(f)
+
+class Report():
+
+    def __init__(self, data_input):
+        self.data_input = data_input
+
+    # Show the Most Expensive Item
+    def get_most_expensive(self):
+        return_price = 0
+
+        for i in data_file:
+            if i["price"] > return_price:
+                return_price = i["price"]
+
+        print("Most Expensive Item: {}".format(return_price))
+
+    
+    # Show the Least Expensive Item
+
+    def get_least_expensive(self):
+        return_price = 0
+
+        for i in data_file:
+            if i["price"] < return_price:
+                return_price = i["price"]
+
+        print("Least Expensive Item: {}".format(return_price))
+
+
+    # Show the Total Revenue
+
+    def get_total_revenue(self):
+        total_revenue = 0
+
+        for i in data_file:
+            total_revenue += i["price"] * i["sold"]
+
+        print("Total Revenue: {}".format(total_revenue))
+
+    # Show the Total Profit
+
+    def get_total_profit(self):
+        total_profit = 0
+
+        for i in data_file:
+            total_profit += (i["price"] - i["cost_to_make"]) * i["sold"]
+
+        print("Total Profit: {}".format(total_profit))
+
+    # Show the 10 Best Sellers.
+    def get_10_best_sellers(self):
+        print("Best Sellers")
+
+        best_sellers = []
+        top_sold = 0
+
+        for i in data_file:
+
+            # Just append the first ten records.
+            if len(best_sellers) < 10:
+                best_sellers.append(i)
+
+            # Compare 'sold' to every best seller
+            else:
+                for best in best_sellers:
+                    if i["sold"] > best["sold"]:
+                        best = i
+
+        for seller in best_sellers:
+            print("{} {}".format(seller["name"], seller["sold"]))
+
+
+    # Show the Number Sold by Each Department
+    def get_sales_by_dept(self):
+        print("Sales By Department")
+
+        for i in data_file:
+            print("{} : {}".format(i["department"], i["sold"]))
+
+
+report = Report(data_file)    
+report.get_most_expensive()
+report.get_least_expensive()
+report.get_total_revenue()
+report.get_total_profit()
+report.get_10_best_sellers()
+report.get_sales_by_dept()
+
+
+
